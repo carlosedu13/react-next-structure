4f4251e2b22c7594d56e0b895b6e0fe1525dc704
Carlos Eduardo <carlosedu.waldorf@gmail.com>
2020-10-03 02:00:32 -0300

    Adding trailingslash and setting x frame options

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 const withImages = require('next-images');
 module.exports = withImages({
   esModule: true,
+  trailingSlash: true,
+  async Headers() {
+    return [
+      {
+        source: '/:path*/',
+        headers: {
+          key: 'X-Frame-Options',
+          value: 'DENY'
+        }
+      }
+    ]
+  }
 });
